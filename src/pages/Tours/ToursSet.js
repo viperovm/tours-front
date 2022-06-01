aad15e10fa8170a10e39840992a588dc8917d316
viperovm <viperovm@gmail.com>
2022-06-01 13:48:34 +0300

    expert page 2 + build

--- src/pages/Tours/ToursSet.js
+++ src/pages/Tours/ToursSet.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './Tours.module.css';
 import Tour from "./Tour";
+import AliceCarousel from "react-alice-carousel";
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 3 },
+};
 
 const ToursSet = ({tours = [], action}) => {
 
@@ -10,6 +17,23 @@ const ToursSet = ({tours = [], action}) => {
         <div className='wrapper'>
           <div className={styles.tours_set_section}>
             {tours && tours.map((tour, index) => <Tour key={index} tour={tour}/>)}
+
+            {tours.length <= 3 && <>
+              <div className={styles.tours_wrapper}>
+                {tours && tours.map((tour, index) => <Tour key={index} tour={tour}/>)}
+              </div>
+
+            </>}
+
+            {tours.length > 3 && <AliceCarousel
+              mouseTracking
+              infinite
+              items={tours && tours.map((tour, index) => <Tour key={index} tour={tour}/>)}
+              responsive={responsive}
+              controlsStrategy="alternate"
+              disableDotsControls
+            />}
+
           </div>
         </div>
       </section>
